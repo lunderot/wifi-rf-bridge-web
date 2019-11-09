80dea461a74940b811c199633d5c234f272713c2
lunderot <mattias.lunderot@gmail.com>
2019-11-09 02:36:32 +0100

    Code is packed into a single file

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-window.onload = () => {
+document.addEventListener('DOMContentLoaded', () => {
 	console.log("Hello world!");
-}
+	document.querySelectorAll('.button').forEach(el => {
+		el.addEventListener('click', event => {
+			event.srcElement.classList.add('is-loading')
+		});
+	});
+});
