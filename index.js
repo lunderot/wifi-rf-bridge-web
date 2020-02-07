c12ea168292f29be059442d066f112bec48fd0c1
lunderot <mattias.lunderot@gmail.com>
2020-02-07 16:14:08 +0100

    Added setting the state of a plug

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dataToDom = (data) => {
 		const stateClass = item.state ? 'is-success' : 'is-danger';
 		const stateText = item.state ? 'On' : 'Off';
 		document.querySelector('#list').innerHTML += `
-			<div class="box columns is-vcentered">
+			<div class="box columns is-vcentered" data-plug=${JSON.stringify(item)}>
 				<div class="column is-half">
 					<p class="title">${item.name}</p>
 				</div>
@@ -29,13 +29,24 @@ const dataToDom = (data) => {
 				event.srcElement.classList.remove('is-success');
 				event.srcElement.textContent = 'Off';
 			}
+			const plug = JSON.parse(event.srcElement.closest('.box').dataset.plug);
+			plug.state = plug.state ? 0 : 1;
+			const plugString = JSON.stringify(plug);
+			event.srcElement.closest('.box').dataset.plug = plugString;
+
+			fetch('/set', {
+				method: 'POST',
+				body: plugString
+			})
+				.then(response => response.text())
+				.then(data => { console.log(data); })
 		});
 	});
 };
 
 document.addEventListener('DOMContentLoaded', () => {
 	fetch('/get')
-	.then(response => response.json())
-	.then(data => dataToDom(data))
-	.catch(error => console.log(error));
+		.then(response => response.json())
+		.then(data => dataToDom(data))
+		.catch(error => console.log(error));
 });
